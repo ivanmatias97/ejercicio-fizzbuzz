4fe8948851564358bb8b0a4925bd930059601546
ivanmatias97 <oscuro2035@gmail.com>
2025-10-02 11:06:47 -0600

    termino primer ciclo archivo main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 cuenta=1
 while cuenta <=1000:
-    if cuenta
+    if cuenta%3==0:
+        print("Fizz")
+    if cuenta%5==0:
+        print("Buzz")
+    if cuenta%15==0:
+        print("FizzBuzz")
+    else:
+        print(cuenta)
+    cuenta+=1
